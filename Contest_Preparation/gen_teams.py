e81cc229565e25ba089b92fa5bc13d14cb188bb5
zhongzihao <642524394@qq.com>
2021-11-18 18:56:21 +0800

    rewrite gen_teams

--- Contest_Preparation/gen_teams.py
+++ Contest_Preparation/gen_teams.py
@@ -1,56 +1,115 @@
 import argparse
 import random
 import utils
+import csv
+import json
+
+
+def gen_password(length):
+    ban_char = '01269CIKOPQSUVWXZbcgiklopsuvwxz'  # Ban confusing characters
+    charset = [chr(j) for j in range(ord('A'), ord('Z') + 1)] + \
+              [chr(j) for j in range(ord('a'), ord('z') + 1)] + \
+              [chr(j) for j in range(ord('0'), ord('9') + 1)]
+    charset = [ch for ch in charset if ch not in ban_char]
+    password = ''
+    for j in range(length):
+        password += charset[random.randint(0, len(charset) - 1)]
+    return password
 
 
 if __name__ == '__main__':
+    gen_password(0)
     parser = argparse.ArgumentParser(description='Generate teams, users, and add them to DOMjudge.')
-    parser.add_argument('team', type=int, help='Team number')
-    parser.add_argument('group_id', type=int, help='Group id')
-    parser.add_argument('--pwd-len', type=int, default=12, help='Password length')
+    parser.add_argument('group_id', type=int, help='Group id to register teams.')
+    parser.add_argument(
+        'max_team_id',
+        type=int,
+        help='Maximum team id that currently exists. You should manually '
+             'check it from DOMjudge/database to avoid duplication.'
+    )
+    parser.add_argument(
+        '--team-csv',
+        type=str,
+        default='team_info.csv',
+        help='Team csv file path. Default: team_info.csv. Refer to the sample file to check its format.'
+    )
+    parser.add_argument(
+        '--seats',
+        type=str,
+        default='seats.txt',
+        help='Location file path. Default: seats.txt. File that lists all available seats.'
+    )
+    parser.add_argument('--pwd-len', type=int, default=12, help='Password length. Default: 12.')
     args = parser.parse_args()
 
+    with open(args.seats, 'r', encoding='utf-8') as fpin:
+        seats = list()
+        for line in fpin:
+            line = line.strip()
+            if line:
+                seats.append(line)
+        random.shuffle(seats)
 
-    with open('teams2.tsv', 'w') as fpout:
-        fpout.write('File_Version\t2\n')
-        for i in range(args.team):
-            team_id = 10 + i  # The 10 is tricky. You should make them unique (different with existed accounts, such as admin), and be the same as team_name.
-            external_id = team_id
-            category_id = [args.group_id]
-            team_name = f'team{team_id:03d}'
-            institution_name = '北京航空航天大学'
-            institution_short_name = 'BUAA'
-            country_code = 'CHN'
-            external_institution_id = 'You said it can be empty???'  # dummy
-            fpout.write('\t'.join([str(team_id), str(external_id), str(category_id), team_name, institution_name,
-                                   institution_short_name, country_code, external_institution_id]) + '\n')
+    max_team_id = args.max_team_id
+    teams = list()
+    accounts = [['accounts', '1']]
+    participant_info = [[
+        '学号',
+        '姓名',
+        '用户名',
+        '密码',
+        '座位',
+    ]]
+    with open(args.team_csv, 'r', encoding='utf-8') as fpin:
+        for index, team in enumerate(csv.reader(fpin)):
+            team_name = f'{team[0]}-{team[1]}'
+            team_id = max_team_id + 1 + index
+            seat = seats[index]
+            teams.append({
+                'id': team_id,
+                'group_ids': [args.group_id],
+                'name': team_name,
+                'display_name': team_name,
+                'organization_id': 'BUAA',
+                'location': seat,
+            })
 
-    with open('teams2.tsv', 'r') as fpin:
+            password = gen_password(args.pwd_len)
+            username = f'team{team_id:04d}'  # Team id should be a prefix of username
+            accounts.append([
+                'team',
+                team[1],  # Full name
+                username,
+                password,
+            ])
+
+            participant_info.append([
+                team[0],  # Student number
+                team[1],  # Full name
+                username,
+                password,
+                seat,
+            ])
+
+    with open('teams.json', 'w', encoding='utf-8') as fpout:
+        json.dump(teams, fpout)
+
+    with open('accounts.tsv', 'w', encoding='utf-8') as fpout:
+        csv.register_dialect('tsv_dialect', delimiter='\t')
+        writer = csv.writer(fpout, dialect='tsv_dialect')
+        writer.writerows(accounts)
+
+    with open('participant_info.csv', 'w', encoding='gbk') as fpout:
+        writer = csv.writer(fpout)
+        writer.writerows(participant_info)
+
+    with open('teams.json', 'r') as fpin:
         response = utils.post('users/teams', files={
-            'tsv': fpin,
+            'json': fpin,
         })
-        print(response.json())
-        response.raise_for_status()
-
-    with open('accounts.tsv', 'w') as fpout:
-        fpout.write('accounts\t1\n')
-        for i in range(args.team):
-            user_type = 'team'
-            full_name = f'team{10 + i:03d}'
-            username = full_name
-            ban_char = '0129IOQZ'  # Ban confusing characters
-            charset = [chr(j) for j in range(ord('A'), ord('Z') + 1)] + \
-                      [chr(j) for j in range(ord('a'), ord('z') + 1)] + \
-                      [chr(j) for j in range(ord('0'), ord('9') + 1)]
-            charset = [ch for ch in charset if ch not in ban_char]
-            password = ''
-            for j in range(args.pwd_len):
-                password += charset[random.randint(0, len(charset) - 1)]
-            fpout.write('\t'.join([user_type, full_name, username, password]) + '\n')
-
+        print(utils.parse_response(response))
     with open('accounts.tsv', 'r') as fpin:
         response = utils.post('users/accounts', files={
             'tsv': fpin,
         })
-        print(response.json())
-        response.raise_for_status()
+        print(utils.parse_response(response))
